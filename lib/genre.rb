ad88c69edd5f6984d591e003292217f846b09300
cedric-joaquin <joaquin.cedric@gmail.com>
2020-06-06 20:02:30 -0700

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,31 @@
+class Genre
+    extend Concerns::Findable
+    attr_accessor :name, :songs
+    @@all = []
+
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def save
+        @@all << self
+    end
+
+    def artists
+        songs.collect {|song| song.artist}.uniq
+    end
+
+    def self.create(name)
+        Genre.new(name).tap {|genre| genre.save}
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+end
