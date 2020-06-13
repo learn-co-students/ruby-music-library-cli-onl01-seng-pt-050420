20bd50a93fa429d57dd36ce63982d3fbc612a424
ryancdavey1 <ryancdavey1@gmail.com>
2020-06-12 18:00:44 -0600

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist
+  @@all= []
+
+  def initialize(name, artist=nil)
+    @name = name
+    #@artist = artist
+    self.artist=(artist)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+  def artist
+    @artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+end
