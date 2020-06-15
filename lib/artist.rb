f45f2f113f8db2fb1f1ca992782a449f75affe29
Catheryne Small <catheryn.small@gmail.com>
2020-06-15 05:32:20 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,55 @@
+class Artist 
+  extend Concerns::Findable
+  attr_accessor :name 
+  attr_reader :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def save
+    self.class.all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    all.clear
+  end
+  
+  def self.create(artist)
+    artist = new(artist)
+    artist.save
+    artist
+  end
+  
+  # def songs
+  #   # @songs
+  #   Songs.all.select{|song| song.artist == self}
+  # end
+  
+  def add_song(song)
+    song.artist = self unless song.artist
+    songs << song unless songs.include?(song)
+  end
+  
+  def genres
+    # song_list = Song.all.select {|song| song.artist == self}
+    # song_list.map(&:genre).uniq # interates through all the artist's songs to find the genre, then only returns unique genres.
+    
+     songs.map(&:genre).uniq
+  end
+end
+
+
+
+
+
+
+
+
+
---
+++ lib/genre.rb
@@ -0,0 +1,40 @@
+class Genre 
+  extend Concerns::Findable
+  attr_accessor :name 
+  attr_reader :songs
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def save
+    @@all << self  
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
+  def self.create(genre)
+    genre = Genre.new(genre)
+    genre.save
+    genre
+  end
+  
+  # def songs
+  #   #@songs
+  #   Song.all {|song| song.genre == self}
+  # end
+  
+  def artists
+    #song_list = Song.all {|song| song.genre == self}
+    #song_list.map(&:artist).uniq
+    songs.map(&:artist).uniq
+  end
+end
